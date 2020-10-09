d22f7c34072a480f0a19ba34fdce3a617cd14f40
Takeshi Yaeda <t.yaeda@gmail.com>
2020-10-10 01:04:57 +0900

    feat: show relative time if it's more then one year old

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -4,6 +4,7 @@ import Head from "next/head";
 import DateTime from "../../components/date-time";
 import utilStyles from "../../styles/utils.module.css";
 import { GetStaticProps, GetStaticPaths } from "next";
+import { formatDistanceToNow, isBefore, parseISO, sub } from "date-fns";
 
 export default function Post({
   postData,
@@ -14,6 +15,8 @@ export default function Post({
     contentHtml: string;
   };
 }) {
+  const postedDate = parseISO(postData.date);
+  const aYearAgoDate = sub(Date.now(), { years: 1 });
   return (
     <Layout>
       <Head>
@@ -23,6 +26,12 @@ export default function Post({
         <h1 className={utilStyles.headingXl}>{postData.title}</h1>
         <div className={utilStyles.lightText}>
           <DateTime dateString={postData.date} />
+          {isBefore(postedDate, aYearAgoDate) ? (
+            <span>
+              {" "}
+              ({formatDistanceToNow(postedDate, { addSuffix: true })})
+            </span>
+          ) : null}
         </div>
         <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
       </article>
