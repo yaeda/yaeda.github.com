c17a09ef7c98f0cc98b9ee868f038c4b551b4d67
Takeshi Yaeda <t.yaeda@gmail.com>
2020-10-13 01:40:52 +0900

    fix handling edge case of CodeBlock

--- components/code-block.tsx
+++ components/code-block.tsx
@@ -3,7 +3,11 @@ import { a11yDark } from "react-syntax-highlighter/dist/cjs/styles/hljs";
 import styles from "./code-block.module.css";
 
 export const CodeBlock = ({ language, value }) => {
-  const [lang, title] = language.split(":");
+  if (value === undefined) {
+    return null;
+  }
+
+  const [lang, title] = language ? language.split(":") : [undefined, undefined];
   return (
     <figure className={styles.figure}>
       {title ? (
